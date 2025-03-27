1e6a5e473134668bc7238e63a6441e8323c650dd
medlak <lakasmohamed0303@gmail.com>
2025-03-28 00:16:18 +0100

    updated portfolio design

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+// Select menu icon and navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+// Toggle menu visibility on click
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 //scroll sections
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
-window.onscroll = () => {
+
+window.addEventListener("scroll", () => {
+  let scrollPosition = window.scrollY;
+
   sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 100;
+    let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
     let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
+    if (scrollPosition >= offset && scrollPosition < offset + height) {
+      navLinks.forEach((link) => link.classList.remove("active"));
+
+      let activeLink = document.querySelector(`header nav a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add("active");
+      }
     }
   });
-};
+  //REMOVE THE MENU BUTTON AND THE NAVBAR AFTER CHOSSING ONE OF THE PAGES
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+});
